c5a262f4ff7b9449578000ff777c4417101e5d3f
OhSSangHoon <gnsdkaos99@naver.com>
2023-05-24 01:57:35 +0900

    algorithm5

--- JavaCollectionFramework/src/HW1.java
+++ JavaCollectionFramework/src/HW1.java
@@ -6,105 +6,138 @@ public class HW1{
 		
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String fileName = sc.next();
-		int targetUser = sc.nextInt(); //대상 사용자의 id
-		int topN = sc.nextInt(); // 유사도 상위 n명의 수
-		int topK = sc.nextInt(); // 상위 k개의 추천 콘텐츠 수
+		String fileName = sc.next(); //파일 이름
+		int targetUser = sc.nextInt(); //대상 사용자의 ID 0,1,2...
+		int topN = sc.nextInt(); //유사도가 높은 상위n명
+		int topK = sc.nextInt(); //추천 컨텐츠 상위k개
 		
 		try {
-			File file = new File(fileName);
-			Scanner fileScanner = new Scanner(file);
-			ArrayList<String> contents = new ArrayList<>();
-			HashMap<String, Integer> contentScores = new HashMap<>(); //콘텐츠를 평가 데이터를 저장하는 HashMap
-			HashMap<String, Integer> contentCounts = new HashMap<>(); //콘텐츠를 평가 데이터를 저장하는 HashMap
-			HashMap<String, Integer> targetUserContentScores = new HashMap<>(); //콘텐츠를 평가 데이터를 저장하는 HashMap
-			HashMap<Integer, HashMap<String, Integer>> userContentScores = new HashMap<>(); //콘텐츠를 평가 데이터를 저장하는 HashMap
+			File file = new File(fileName); //fileName으로 부터 입력받은 flie을 읽어온다.(일치할 시)
+			Scanner fileScanner = new Scanner(file); //해당 file을 스캔 //32L부터
+
+			ArrayList<String> contents = new ArrayList<>(); 
+			//file의 콘텐츠를 임시 저장, 정규화된 점수 계산 및 출력에 사용
+			
+			//HashMap 사용 이유 빠른 접근 시간, 중복 데이터 처리(콘텐츠나 사용자ID), 데이터 추적 및 분석(콘텐츠의 등장 횟수, 사용자별 콘텐츠 점수)에 용이함
+			
+			HashMap<String, Integer> contentScores = new HashMap<>();
+			//콘텐츠와 그에 대한 점수(등장횟수 + 평가점수)를 저장 하는 맵
+			
+			HashMap<String, Integer> contentCounts = new HashMap<>();
+			//콘텐츠의 등장횟수를 추척재 맵에 저장하고 콘텐츠 평균 점수를 구하는데 사용
+			
+			HashMap<String, Integer> targetUserContentScores = new HashMap<>();
+			//대상 사용자의 콘텐츠와 점수를 저장하는 맵, 대상 사용자가 평가한 콘텐츠와 그에 대한 점수를 저장
+			
+			HashMap<Integer, HashMap<String, Integer>> userContentScores = new HashMap<>();
+			//사용자별 콘텐츠와 점수를 저장하는 맵,
+			//사용자ID를 key로 사용하여 해당 사용자가 평가한 콘텐츠와 그에 대한 점수를 저장 유사도 계산에 활용
 			
 
 			
-			if(fileScanner.hasNextLine()) {
+			if(fileScanner.hasNextLine()) {//첫 번째 줄을 건너뛰고 두 번째 줄부터 읽음
 				fileScanner.nextLine();
 			}
 			
-			int userContentSum = 0;
+			int userContentSum = 0;//대상 사용자의 콘텐츠 점수 총합을 저장
 			
-			//텍스트 파일에서 데이터를 읽어옴(txt파일에서 읽어온 값을 저장한 후,  )
-			while(fileScanner.hasNextLine()) {
-				String line = fileScanner.nextLine();
-				//각줄의 데이터를 공백으로 구분하여 배열에 저장
-				String[] parts = line.split(" ");				
-				int user = Integer.parseInt(parts[0]);//user
+			//file의 각 줄의 데이터를처리
+			while(fileScanner.hasNextLine()) {//다음 줄 확인
+				String line = fileScanner.nextLine();//읽어온 데이터를 line에 저장
+				String[] parts = line.split(" ");//line을 공백을 기준으로 나눠서 parts에 저장 
+				int user = Integer.parseInt(parts[0]);//사용자
 				String content = parts[1];//content
 				int score = Integer.parseInt(parts[2]);//score를 저장
 				
-				//현재user가 targetUser라면
+				//현재user가 targetUser라면(targetUser의 콘텐츠와 점수를 찾고 컨텐츠 점수 누적합)
 				if(user == targetUser) {
-					//targetUser의 content점수를 contents변수에 추가
+					//targetUser의 콘텐츠를 arrayList contents에 추가
 					contents.add(content);
+					
 					//HashMap에 콘텐츠와 점수 저장
 					targetUserContentScores.put(content, score);
 					userContentSum += score; //누적합
 				}
 				
-				//HashMap에 content와, contentScores(등장횟수 + score)를 저장
-				contentScores.put(content, contentScores.getOrDefault(content, 0) + score);
-				contentCounts.put(content, contentCounts.getOrDefault(content, 0) + 1);
-				//콘텐츠별로 점수 합계와 등장 횟수를 추적하고, 이 정보를 나중에 평균 점수 계산이나 다른 분석에 사용할 수 있다.
+				//getOrDefault를 사용하여 맵에서 해당 콘텐츠를 키로 가진 값을 가져온다. 만약 해당 콘텐츠가 존재하지 않는다면, 기본값0으로 반환
+				//가져온 점수 합계와 현재 점수(score)를 더하여 새로운 점수 합계를 계산(각 콘텐츠의 등장 횟수와, 점수 합계를 저장, 평균 점수계산에 사용)
+				contentScores.put(content, contentScores.getOrDefault(content, 0) + score);//해당 콘텐츠가 등장한 모든 사용자의 점수 합계를 저장
 				
-				//user값이 존재하는 경우 user값을 반환하고 없을 경우 새로 hashMap을 만들고 null값 반환
+				contentCounts.put(content, contentCounts.getOrDefault(content, 0) + 1 );//(사용자들에 의해 평가된 횟수를 추적함)콘텐츠의 등장횟수를 저장
+				
+				//있으면 user의 데이터가 있는 맵을 가져오고, 없으면 새로운 맵 생성 
 				userContentScores.putIfAbsent(user, new HashMap<>());
 				
-				//user의 키값을 받은 userContentScores에 content와 score를 넣는다.
-				//각 사용자 별로 각 콘텐츠에 대한 점수가 저장
+				//user들이 평가한 콘텐츠를 읽어 content와 점수를 저장 user = key
+				//만약 해당 사용자의 데이터가 존재하지 않으면 (null) putIfAbsent함수를 이용하여 새로운 맵을 만들고
+				//이후에 put실행
 				userContentScores.get(user).put(content, score);
+				
+				//※ part로 분리한 데이터들을 puIfAbsent를 활용해 user별 맵을 만들고 거기에 content와 socres를 집어넣는다.
 			}
 			fileScanner.close();
+			//file을 다 읽었으면 닫는다.(작업완료)
 			
 			
-			//정규화점수 content 오름차순 정렬
+			//문제1.정규화점수 정렬할 대상인 contents들만 가져와서 오름차순 정렬
 			Collections.sort(contents, new Comparator<String>() {
+				//o1과 o2두개의 문자열을 인자로 받는 compare함수
 				public int compare(String o1, String o2) {
-					//content의 문자와 숫자를 구분하기 위해 정규 표현식 사용
+					//content의 문자와 숫자를 구분하기 위해 정규 표현식 사용 ex) 'A'와'0' 
 					Pattern pattern = Pattern.compile("(\\D+)(\\d+)");
 					Matcher m1 = pattern.matcher(o1);
 					Matcher m2 = pattern.matcher(o2);
 					
 					if(m1.find() && m2.find()) {
-						//
+						//m1과 m2를 찾아서 
 						int charCompare = m1.group(1).compareTo(m2.group(1));
-						//문자열이 다른 경우
+						//matcher그룹의(1번 문자열) 문자열이 다른 경우 m1그룹과 m2그룹을 비교
 						if(charCompare != 0) {
-							//m1(1)과 m2(1)의 비교값 반환
-							return charCompare;
+							//m1(문자열)과 m2(문자열)의 비교값 반환 (문자가 다르므로 숫자까지 비교할필요x)
+							return charCompare;// compare에 비교값 반환
 						}else {
-							//문자열이 같은 경우 m1(2)와 m2(2)를 정수형으로 변환하여 비교
+							//문자열이 같은 경우 m1(숫자)와 m2(숫자)를 정수형으로 변환하여 비교
+							//pattern.compile로 정규 표현식에 맞는것을 찾았을 뿐 정수형으로 변한건 아님
 							int num1 = Integer.parseInt(m1.group(2));
 							int num2 = Integer.parseInt(m2.group(2));
-							return Integer.compare(num1, num2);//비교값 반환
+							return Integer.compare(num1, num2);// compare에 비교값 반환
 						}
 					}
-					//반환값을 기준으로 Collections.sort를 통해 정렬
-					return o1.compareTo(o2);
+					return 0;
 				}
 			});
 			
 			
 			System.out.println("1.사용자" + targetUser + "의 콘텐츠와 정규화 점수: ");
 			System.out.print("[");
+			
+			//ArrayList contents에 저장된 각 콘텐츠를 반복적으로 가져와 content에 할당
+			//contents = file데이터를 저장한 arrayList, content = for문으로 현재 가져오고있는 contents의 데이터
 			for(String content: contents) {
+				
 				//정규화 계산식
+				//현재 콘텐츠(content)에 대한 대상 사용자(targetUser)의 점수를 가져온다.
+				//targetUserContentScores.get(content) = 현재 콘텐츠에 대한 targetuser의 contents정보
 				double targetUserScore = targetUserContentScores.get(content);
-		        double userContentAverage = (double) userContentSum / contents.size();
-		        double average = targetUserScore - userContentAverage;
+				
+				//사용자의 콘텐츠 평균 점수 5 + 4 + 1이라면 평균점수는 10/3()
+				//평균 = 콘텐츠점수의 누적합 / 평가한 콘텐츠의 개수
+				double userContentAverage = (double) userContentSum / contents.size();
+				
+				//점수의 정규화 = 대상사용자의 점수 - 평균
+				double average = targetUserScore - userContentAverage;
 		        
-		        double totalContentScore = (double) contentScores.get(content) / contentCounts.get(content);
-		        
+				//출력: 오름차순 정렬을한 content와, 해당 content의 정규화 점수
 				System.out.printf("(%s, %.3f)", content, average);
+				
+				//arrayList contents의 마지막 요소라면 ,출력 (ex: A0의 마지막이라면 ,출력 )
+				//contents.size()에서 -1을 한 값이 contents의 마지막 요소
 				if(contents.indexOf(content) != contents.size() - 1) {
 					System.out.print(", ");
 				}
 			}
 			System.out.print("]\n\n\n");
+			//여기까지함
 			
 			//userAverages 맵 생성
 			HashMap<Integer, Double> userAverages = new HashMap<>();
@@ -154,6 +187,7 @@ public class HW1{
 				System.out.printf("사용자 id: %d, 유사도: %.6f\n", entry.getKey(), entry.getValue());
 			}
 			System.out.print("\n\n");
+			
 			
 			
 			System.out.println("3. 사용자" + targetUser +"에게 추천할 콘텐츠와 추천 점수");
--- algorithm-5/src/KMPAlgorithm.java
+++ algorithm-5/src/KMPAlgorithm.java
@@ -1,5 +1,5 @@
 public class KMPAlgorithm {
-
+ 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
