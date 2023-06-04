97b14e499ccf2bbe9283e4783b021af25a0abeb8
OhSSangHoon <gnsdkaos99@naver.com>
2023-06-04 17:35:17 +0900

    TF-IDF

--- TF-IDF/src/HW2.java
+++ TF-IDF/src/HW2.java
@@ -1,31 +1,69 @@
 import java.io.*;
 import java.util.*;
 
-
 public class HW2 {
+    public static void main(String[] args) {
+        Map<String, List<String>> dataMap = new HashMap<>();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String filename = sc.nextLine();
-		
-		BufferedReader reader = null;
-		
-		try {
-			
-			reader = new BufferedReader(new FileReader(filename));
-			String fileScanner = null;
-			
-			while((fileScanner = reader.readLine()) != null) {
-				System.out.println(fileScanner);
-			}
-			
-		}catch(FileNotFoundException e) {
-			System.out.println("File 찾기 실패");
-		}
-		
-		catch (IOException e) {
-			System.out.println("file 읽어오기 실패");
-		}
-	}
+        Scanner sc = new Scanner(System.in);
 
+        String filename = sc.next();
+        String targetTitle = sc.next().toLowerCase();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            String title = null;
+
+            while ((line = reader.readLine()) != null) {
+                if (title == null) {
+                    title = line.toLowerCase();
+                    dataMap.put(title, new ArrayList<>());
+                } else {
+                    String content = line.toLowerCase();
+                    dataMap.get(title).add(content);
+                    title = null;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Error reading the file");
+        }
+
+        if (dataMap.containsKey(targetTitle)) {
+            List<String> contents = dataMap.get(targetTitle);
+            int numDocuments = dataMap.size();
+            int totalContents = 0;
+
+            for (List<String> contentList : dataMap.values()) {
+                totalContents += contentList.size();
+            }
+
+            for (String content : contents) {
+                Map<Integer, Integer> termFrequencies = calculateTermFrequencies(content);
+                System.out.println("TF Vector:");
+
+                for (Map.Entry<Integer, Integer> entry : termFrequencies.entrySet()) {
+                    int termHash = entry.getKey();
+                    int tf = entry.getValue();
+                    double tfNormalized = (double) tf / totalContents;
+                    System.out.println(termHash + ": " + tfNormalized);
+                }
+            }
+        } else {
+            System.out.println("Title not found.");
+        }
+    }
+
+    private static Map<Integer, Integer> calculateTermFrequencies(String content) {
+        Map<Integer, Integer> termFrequencies = new HashMap<>();
+        String[] words = content.split("[,.?!:\"\\s]+");
+
+        for (String word : words) {
+            int termHash = word.hashCode();
+            termFrequencies.put(termHash, termFrequencies.getOrDefault(termHash, 0) + 1);
+        }
+
+        return termFrequencies;
+    }
 }
